90911e901b2e92523f13697e2b3eb54747fdcfc5
Carlton Brady <ctombrady@gmail.com>
2018-05-14 22:59:03 -0400

    Added ability to print the state of a board

---
+++ include/coordinates.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+class Coordinates{
+  public:
+    int x, y;
+    Coordinates(int initX, int initY);
+};
---
+++ src/coordinates.cpp
@@ -0,0 +1,6 @@
+#include "../include/coordinates.hpp"
+
+Coordinates::Coordinates(int initX, int initY){
+  x = initX;
+  y = initY;
+}
