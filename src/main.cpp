8f59269572b6741b9cd9841cf445b92f90aef810
Carlton Brady <ctombrady@gmail.com>
2018-07-08 18:23:08 -0400

    Cleaning up comments

--- src/main.cpp
+++ src/main.cpp
@@ -11,45 +11,12 @@
 #include "../include/humanPlayer.hpp"
 #include "../include/action.hpp"
 #include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
 int main() {
-  // vector<int> v;
-  // Ship myShip("hello world",4);
-  // Ship *myShipPtr = &myShip;
-  // myShip.takeHit();
-  // Space mySpace;
-  // Board board(10,10);
-  // Coordinates coordinates(0,3);
-  //
-  // board.placeShip(myShipPtr, coordinates, "south");
-  // cout << mySpace.isEmpty() << endl;
-  // cout << myShip.hp << endl;
-  // cout << board.printPlayerBoardState() << endl;
-  //
-  // cout << board.isValidPlacement(myShipPtr, coordinates, "north") << endl;
-  // cout << board.isValidPlacement(myShipPtr, coordinates, "south") << endl;
-  // cout << board.isValidPlacement(myShipPtr, coordinates, "east") << endl;
-  // cout << board.isValidPlacement(myShipPtr, coordinates, "west") << endl;
-  // cout << board.ships[0].name << endl;
-  // cout << board.allShipsSunk() << endl;
-  //
-  // vector<string> tokens = stringCommaTokenize("hello,world");
-  //
-  // cout << tokens[0] << endl;
-  // cout << tokens[1] << endl;
-  // Game game = Game();
-  // game.initializeGame();
-  // game.player2->doInitialShipPlacements();
-  // int xCoord = rand()%10;
-  // int yCoord = rand()%10;
-  // Coordinates targetCoords(xCoord,yCoord);
-  // Action* action = new Action(targetCoords,game.player2->board);
-  // action->takeAction();
-  // cout << game.player2->board->printPlayerBoardState() << endl;
-
-
+  srand(time(0));
   // Initializing, setting up, and playing the game:
   Game game = Game();
   game.initializeGame();
